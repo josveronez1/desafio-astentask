3d8dee2e2710b9a24262f8ab407d994895b735fe
josveronez1 <joseveronezzz@gmail.com>
2026-03-06 10:18:50 -0300

    Primeiro commit: Criação do projeto com Spring Initializr - Dependenciees: Spring Security, PostgreSQL Driver, Spring Data JPA, Spring Web Criação da estrutura de pastas

---
+++ src/main/java/com/josveronez/desafio_astentask/DesafioAstentaskApplication.java
@@ -0,0 +1,13 @@
+package com.josveronez.desafio_astentask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioAstentaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioAstentaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/josveronez/desafio_astentask/DesafioAstentaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josveronez.desafio_astentask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioAstentaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
